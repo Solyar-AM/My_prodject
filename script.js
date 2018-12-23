3a7943cd8d33ff996040504119f712d16450b189
Solyar-AM <46029858+Solyar-AM@users.noreply.github.com>
2018-12-23 23:23:35 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,40 @@
+var myNodelist = document.getElementsByTagName('li');
+for (var i = 0; i < myNodelist.length; i++) {
+    createCloseButton(myNodelist[i]);
+}
+
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+    if (ev.target.tagName === 'LI') {
+        ev.target.classList.toggle('checked');
+    }
+}, false);
+function newElement() {
+    var inputValue = document.getElementById("myInput").value;
+    if (inputValue === '') {
+        alert("The field dont't be empty!");
+    } else {
+        //Создаем новый элемент li
+        var li = document.createElement("li");
+        var t = document.createTextNode(inputValue);
+        li.appendChild(t);
+        document.getElementById('myUL').appendChild(li);
+
+        //Делаем кнопку для удаления (крестик)
+        createCloseButton(li);
+
+        //Чистим поле input
+        document.getElementById('myInput').value = "";
+    }
+}function createCloseButton(node) {
+    var span = document.createElement("span");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    node.appendChild(span);
+    span.onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
+}
+    
